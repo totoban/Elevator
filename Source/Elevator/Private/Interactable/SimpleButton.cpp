38f85d1f148d17a68053001037b0bd3ae0c2ca13
tom <raitener666@gmail.com>
2024-08-13 13:53:42 +0700

    Cours 1.3 Création de la première classe

---
+++ Source/Elevator/Private/Interactable/SimpleButton.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interactable/SimpleButton.h"
+
+// Sets default values
+ASimpleButton::ASimpleButton()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASimpleButton::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASimpleButton::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Elevator/Public/Interactable/SimpleButton.h
@@ -0,0 +1,27 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SimpleButton.generated.h"
+
+UCLASS()
+class ELEVATOR_API ASimpleButton : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASimpleButton();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+};
