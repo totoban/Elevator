57d4dabc323ce2fc03f89d2616bffad38e81d949
tom <raitener666@gmail.com>
2024-08-18 14:15:21 +0700

    1.6 On Component Begin Overlap

--- Source/Elevator/Private/Interactable/SimpleButton.cpp
+++ Source/Elevator/Private/Interactable/SimpleButton.cpp
@@ -19,13 +19,22 @@ ASimpleButton::ASimpleButton()
 
 	BoxCollision = CreateDefaultSubobject<UBoxComponent>("Box Collision");
 	BoxCollision->SetupAttachment(ButtonMesh);
+	BoxCollision->SetBoxExtent(FVector(20.f, 20.f, 7.5));
+	BoxCollision->SetRelativeLocation(FVector(0.f, 0.f, 20.f));
+	BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ASimpleButton::OnBeginOverlap);
 	
+}
+
+void ASimpleButton::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OVERLAP BEGIN"))
 }
 
 // Called when the game starts or when spawned
 void ASimpleButton::BeginPlay()
 {
 	Super::BeginPlay();
+	
 	
 }
 
--- Source/Elevator/Public/Interactable/SimpleButton.h
+++ Source/Elevator/Public/Interactable/SimpleButton.h
@@ -17,6 +17,12 @@ public:
 	// Sets default values for this actor's properties
 	ASimpleButton();
 
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+		const FHitResult& SweepResult);
+
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
